1470155058bd1212d23b105162fe22b249c15a90
Tiago Henriques <trinosauro@gmail.com>
2012-04-16 19:41:11 +0100

    git: copied several github gitignores into my global gitignore

---
+++ gitignore.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+GITIGNORE_DIR="$HOME/github/gitignore"
+for i in Django.gitignore Global/OSX.gitignore Global/SublimeText.gitignore Global/TextMate.gitignore Global/vim.gitignore Objective-C.gitignore Python.gitignore
+do
+    echo -e "## BEGIN https://github.com/github/gitignore/blob/master/$i\n"
+    cat $GITIGNORE_DIR/$i
+    echo -e "## END   https://github.com/github/gitignore/blob/master/$i\n"
+done
