3503ef86daaeb3de8b8e0a0509c0bc979d8486b1
Tiago Henriques <the@sinnerschrader-mobile.com>
2013-11-13 15:47:57 +0100

    bash: added scripts to pull or fetch all repos inside a directory

---
+++ bin/fetch_all.sh
@@ -0,0 +1,1 @@
+for i in $(ls -d */); do oldcd=$(pwd); cd "${i}"; pwd; git fetch --all; cd "${oldcd}"; done
---
+++ bin/pull_all.sh
@@ -0,0 +1,1 @@
+for i in $(ls -d */); do oldcd=$(pwd); cd "${i}"; pwd; git pull; cd "${oldcd}"; done
